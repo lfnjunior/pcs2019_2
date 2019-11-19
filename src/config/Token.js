7fa7139ba2a4145240fdd1ad18559acb069384ef
lfnjunior <leonaldofnunes@gmail.com>
2019-11-19 19:04:01 -0300

    Client and Server

--- src/config/Token.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken")
-const authConfig = require('./auth')
-
-module.exports = {
-    generateToken(params) {
-        return jwt.sign( params , authConfig.secret , { expiresIn: '1h' })         
-    }
-}
